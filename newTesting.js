f1300c87be4a753078261901f0de6496ba6ca49b
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2021-10-05 21:12:22 +0500

    Update newTesting.js

--- newTesting.js
+++ newTesting.js
@@ -1,5 +1,5 @@
 let now = new Date;
-        let next = new Date("2021/11/08");
+        let next = new Date("2021/10/08");
         let ownVal = (next - now) / (1000 * 60 * 60 * 24);
         ownVal = Math.floor(ownVal);
         let ownF = ownVal * (0.033);
